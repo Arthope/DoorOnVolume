f718bf0d49f096fff0523db4de68d7157d15b7d1
Arthope <pochtawow345@gmail.com>
2022-08-05 16:38:38 +0300

    Init

---
+++ Assets/Scripts/door/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+
+    public event UnityAction<int> HealthChenged;
+    public int Health => _health;
+
+    public void Start()
+    {
+        HealthChenged?.Invoke(_health);
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        _health -= damage;
+        HealthChenged?.Invoke(_health);
+
+        if (_health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+
+    }
+}
---
+++ Assets/Scripts/door/VolumeValue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class VolumeValue : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private float _maxVolume = 1;
+    private float _musicVolume = 0.1f;
+    private float _minVolume = 0.1f;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        StartCoroutine(VolumeChange()); ;
+    }
+
+    private IEnumerator VolumeChange()
+    {
+        _audioSource.volume = _musicVolume;
+        while (true)
+        {
+            if (_musicVolume < _maxVolume)
+            {
+                _musicVolume = Mathf.MoveTowards(_minVolume, _maxVolume, _musicVolume * Time.deltaTime);
+            }
+
+            yield return null;
+        }
+    }
+
+}
