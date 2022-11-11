b59e03003cfe89785403057b7e3c03c750335c26
Arthope <pochtawow345@gmail.com>
2022-11-11 13:32:38 +0300

    Update EndPoint.cs

--- Assets/Scripts/door/EndPoint.cs
+++ Assets/Scripts/door/EndPoint.cs
@@ -1,19 +1,34 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-
 public class EndPoint : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private UnityEvent _reached;
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] private float _speed;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Player>(out Player player))
         {
-            _reached?.Invoke();
+            _reached.Invoke();
+            var fadeInJob = StartCoroutine(FadeIn());
+        }
+    }
+    private IEnumerator FadeIn()
+    {
+        var volume = _audioSource.volume;
+        var waitForOneSeconds = new WaitForSeconds(1f);
+
+        for (int i = 0; i < 10; i++)
+        {
+            volume = 1f - (1f / 10f * i);
+            _audioSource.volume = Mathf.MoveTowards(volume, i, _speed);
+            yield return waitForOneSeconds;
         }
     }
 }
+
