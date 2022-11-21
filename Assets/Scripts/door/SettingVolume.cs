ab93b105a3a21bcd3f1dfa3436ecf6777dba3824
Arthope <pochtawow345@gmail.com>
2022-11-21 20:47:43 +0300

    Init

---
+++ Assets/Scripts/door/SettingVolume.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class SettingVolume : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    public void TurnOn()
+    {
+        int targetVolume = 1;
+
+        StartCoroutine(ChangeVolumeSmoothly(targetVolume));
+    }
+
+    public void TurnOff()
+    {
+        int targetVolume = 0;
+
+        StartCoroutine(ChangeVolumeSmoothly(targetVolume));
+    }
+
+    private IEnumerator ChangeVolumeSmoothly(int targetVolume)
+    {
+
+        if (_audioSource.isPlaying == false)
+        {
+            _audioSource.Play();
+        }
+
+        while (_audioSource.volume < targetVolume || _audioSource.volume > targetVolume)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, targetVolume, Time.deltaTime);
+            yield return null;
+        }
+
+        if (_audioSource.volume <= 0 && _audioSource.isPlaying == true)
+        {
+            _audioSource.Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/door/Trigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _activated;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        int numberOfEvent = 0;
+        _activated[numberOfEvent].Invoke();
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        int numberOfEvent = 1;
+        _activated[numberOfEvent].Invoke();
+    }
+}
